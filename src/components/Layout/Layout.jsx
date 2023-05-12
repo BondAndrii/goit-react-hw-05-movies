b2882ad63be0aa50ec3a73c566e5b8706b142431
BondAndrii <104005997+BondAndrii@users.noreply.github.com>
2023-05-12 21:05:44 +0300

    styled layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+
 import { NavLink } from "react-router-dom";
+
 import { Outlet } from "react-router-dom/dist";
-import "./Layout.css"
+
+import styles from "./Layout.module.css"
 
 export const Layout = () => {
     return (
-        <div className="Container"> 
-            <header>
-                <nav>
-                    <NavLink to="/">Home</NavLink>
-                    <NavLink to="/movies">Movies</NavLink>
+        <div className={styles.Container}> 
+            <header className={styles.Header}>
+                <nav className={styles.Navigation}>
+                    <NavLink className={styles.Link} to="/">Home</NavLink>
+                    <NavLink className={styles.Link} to="/movies">Movies</NavLink>
                 </nav>
            </header>
             <Outlet/>
